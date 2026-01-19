6f874ecec6790e6678b37117d34e90d55b802bc6
alex.musca07-cloud <musca@Katy>
2026-01-19 12:51:57 +0100

    Barco y motor

--- reto javier fabricio alex/src/clases/Barco.java
+++ reto javier fabricio alex/src/clases/Barco.java
@@ -3,4 +3,37 @@ package clases;
 public class Barco{
 	protected String nombre;
 	protected double capacidadCombustible;
+	protected Motor motor;
+	protected double calcularAutonomia() {
+		return capacidadCombustible/motor.consumo;
+	}
+	public Barco(String nombre, double capacidadCombustible, Motor motor) {
+		super();
+		this.nombre = nombre;
+		this.capacidadCombustible = capacidadCombustible;
+		this.motor = motor;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getCapacidadCombustible() {
+		return capacidadCombustible;
+	}
+	public void setCapacidadCombustible(double capacidadCombustible) {
+		this.capacidadCombustible = capacidadCombustible;
+	}
+	public Motor getMotor() {
+		return motor;
+	}
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+	@Override
+	public String toString() {
+		return "Barco [nombre=" + nombre + ", capacidadCombustible=" + capacidadCombustible + ", motor=" + motor + "]";
+	}
+	
 	}
