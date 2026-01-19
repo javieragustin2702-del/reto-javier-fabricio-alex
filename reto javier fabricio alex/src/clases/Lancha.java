3ea0561a75dace36f19d9878c93bca7d2028b180
alex.musca07-cloud <musca@Katy>
2026-01-19 12:56:23 +0100

    Lancha

---
+++ reto javier fabricio alex/src/clases/Lancha.java
@@ -0,0 +1,25 @@
+package clases;
+
+public class Lancha extends Barco{
+	protected boolean cubierta;
+
+	public Lancha(String nombre, double capacidadCombustible, Motor motor, boolean cubierta) {
+		super(nombre, capacidadCombustible, motor);
+		this.cubierta = cubierta;
+	}
+
+	public boolean isCubierta() {
+		return cubierta;
+	}
+
+	public void setCubierta(boolean cubierta) {
+		this.cubierta = cubierta;
+	}
+
+	@Override
+	public String toString() {
+		return "Lancha [cubierta=" + cubierta + "]";
+	}
+	
+
+}
