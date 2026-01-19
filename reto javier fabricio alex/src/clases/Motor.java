188974e452271a95923a51c79947a8d3eeff60c3
alex.musca07-cloud <musca@Katy>
2026-01-19 12:34:05 +0100

    Motor

---
+++ reto javier fabricio alex/src/clases/Motor.java
@@ -0,0 +1,15 @@
+package clases;
+
+public class Motor {
+	protected String modelo;
+	protected int potencia;
+	protected  double consumo;
+	public Motor(String modelo, int potencia, double consumo) {
+		super();
+		this.modelo = modelo;
+		this.potencia = potencia;
+		this.consumo = consumo;
+	}
+	
+	
+}
